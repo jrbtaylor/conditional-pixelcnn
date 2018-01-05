bab2027340a8385d7da5846c813e61ef77592d96
jrbtaylor <jasonrbtaylor@gmail.com>
2018-01-05 16:53:31 -0500

    add options to use different datasets, add dataset name to experiment title

--- experiment.py
+++ experiment.py
@@ -13,14 +13,14 @@ import data
 import model
 import train
 
-def run(batch_size=64,n_features=150,n_layers=6,n_bins=4,
-        optimizer='adam',learnrate=1e-4,dropout=0.5,exp_name='pixelCNN',
+def run(dataset='mnist', batch_size=64, n_features=150, n_layers=6, n_bins=4,
+        optimizer='adam', learnrate=1e-4, dropout=0.5, exp_name='pixelCNN',
         exp_dir='/home/jason/experiments/conditional-pixelcnn/',
-        n_classes=10,cuda=True,resume=False):
+        n_classes=10, cuda=True, resume=False):
 
-    exp_name += '_%ifeat_%ilayers_%ibins'%(
-        n_features,n_layers,n_bins)
-    exp_dir = os.path.join(exp_dir,exp_name)
+    exp_name += '_%s_%ifeat_%ilayers_%ibins'%(
+        dataset, n_features, n_layers, n_bins)
+    exp_dir = os.path.join(exp_dir, exp_name)
     if not os.path.isdir(exp_dir):
         os.makedirs(exp_dir)
 
@@ -33,34 +33,30 @@ def run(batch_size=64,n_features=150,n_layers=6,n_bins=4,
             json.dump(params,f)
 
         # Model
-        net = model.PixelCNN(1,n_classes,n_features,n_layers,n_bins,dropout)
+        net = model.PixelCNN(1, n_classes, n_features, n_layers, n_bins,
+                             dropout)
     else:
         # if resuming, need to have params, stats and checkpoint files
         if not (os.path.isfile(os.path.join(exp_dir,'params.json'))
                 and os.path.isfile(os.path.join(exp_dir,'stats.json'))
                 and os.path.isfile(os.path.join(exp_dir,'last_checkpoint'))):
             raise Exception('Missing param, stats or checkpoint file on resume')
-        net = torch.load(os.path.join(exp_dir,'last_checkpoint'))
+        net = torch.load(os.path.join(exp_dir, 'last_checkpoint'))
 
     # Data loaders
-    train_loader,val_loader = data.mnist(batch_size)
+    train_loader,val_loader = data.loader(dataset, batch_size)
 
     # Up-weight 1s (~8x rarer) to balance loss, interpolate intermediate values
-    weight = torch.from_numpy(np.linspace(1,8,n_bins,dtype='float32'))
+    weight = torch.from_numpy(np.linspace(1, 8, n_bins, dtype='float32'))
     if cuda:
         weight = weight.cuda()
 
     # Define loss fcn, incl. label formatting from input
     def input2label(x):
         return torch.squeeze(torch.round((n_bins-1)*x).type(torch.LongTensor),1)
-    def onehot(x):
-        def _onehot(x):
-            y = np.zeros((1,n_classes),dtype='float32')
-            y[0,x] = 1
-            return y
-        return torch.from_numpy(np.concatenate([_onehot(v) for v in x]))
     loss_fcn = torch.nn.NLLLoss2d(torch.autograd.Variable(weight))
 
     # Train
-    train.fit(train_loader,val_loader,net,exp_dir,input2label,onehot,loss_fcn,
-              optimizer,learnrate=learnrate,cuda=cuda,resume=resume)
+    train.fit(train_loader, val_loader, net, exp_dir, input2label, loss_fcn,
+              n_classes, optimizer, learnrate=learnrate, cuda=cuda,
+              resume=resume)
